50c8030988a06cbb673ae9d6c8b086afa9dfece7
Cheryl D. Calhoun <cheryl.calhoun@sfcollege.edu>
2021-02-21 10:52:40 -0500

    Updating Chapter 13
    Uploading results of Chapter 13.

---
+++ Chapter 14/DialogDemo/app/src/main/java/com/example/dialogdemo/MyDialog.java
@@ -0,0 +1,41 @@
+package com.example.dialogdemo;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+
+public class MyDialog extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        // Use the Builder class because this dialog
+        // has a simple UI
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        // Dialog will have "Make a selection" as the title
+        builder.setMessage("Make a selection")
+
+        // An OK button that does nothing
+        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            // @Override
+            public void onClick(DialogInterface dialog, int id) {
+                // Nothing happening here
+            }
+        })
+
+        // A "Cancel" button that does nothing
+        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            // @Override
+            public void onClick(DialogInterface dialog, int id) {
+
+                // Nothing happening here either
+            }
+        });
+
+        // Create the object and return it
+        return builder.create();
+    } // End of onCreateDialog
+}
