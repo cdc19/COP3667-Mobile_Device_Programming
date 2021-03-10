128ab4558e8568866407259ef05db9a6c869a1a2
Cheryl D. Calhoun <cheryl.calhoun@sfcollege.edu>
2021-03-10 13:23:28 -0500

    Add Chapter 15 projects
    Update the repository to include the three chapter 15 apps.

---
+++ DynamicArrayExample/app/src/main/java/com/example/dynamicarrayexample/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.dynamicarrayexample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        int[] ourArray = new int[1000];
+
+        //Let's initialize ourArray using a for loop
+        //Because more than a few variables is allot of typing!
+        for(int i = 0; i < 1000; i++) {
+            //Put the value of ourValue into our array
+            //At the position determined by i.
+            ourArray[i] = i * 5;
+
+            //Output what is going on
+            Log.i("info", "i = " + i);
+            Log.i("info", "ourArray[i] = " + ourArray[i]);
+        }
+    }
+}
