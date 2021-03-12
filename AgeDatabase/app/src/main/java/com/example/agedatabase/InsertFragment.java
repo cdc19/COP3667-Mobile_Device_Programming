3e8d5bf47c5a546a235ddeb30616f52ba7d5833e
Cheryl D. Calhoun <cheryl.calhoun@sfcollege.edu>
2021-03-12 13:04:21 -0500

    Chapter 27 Update
    Add database functionality to AgeDatabase app

--- AgeDatabase/app/src/main/java/com/example/agedatabase/InsertFragment.java
+++ AgeDatabase/app/src/main/java/com/example/agedatabase/InsertFragment.java
@@ -5,15 +5,27 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.fragment.app.Fragment;
 
 public class InsertFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.content_insert, container, false);
+
+        final DataManager dm = new DataManager(getActivity());
+        Button btnInsert = (Button) v.findViewById(R.id.btnInsert);
+        final EditText editName = (EditText) v.findViewById(R.id.editName);
+        final EditText editAge = (EditText) v.findViewById(R.id.editAge);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dm.insert(editName.getText().toString(),
+                        editAge.getText().toString());
+            }
+        });
         return v;
-
-        // Database and UI code goes here in next chapter.
-
     } // End onCreateView method
 } // End InsertFragment class
